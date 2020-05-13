53222e2c4cfd541b31c1c93d1e579b2daa5ebeab
Appbird <project.appbird@gmail.com>
2020-05-14 02:01:37 +0900

    v1.0.5 ファイル読み込み(helperAboutFiles)を修正

--- src/helper/programHelperFunctions/helperAboutFiles.ts
+++ src/helper/programHelperFunctions/helperAboutFiles.ts
@@ -2,10 +2,10 @@ import { readFileSync, writeFileSync } from "fs";
 
 const helperAboutFiles = {
     loadJSONFromlFileInDataBase(fileName:string):any{
-        return JSON.parse(readFileSync(__dirname.replace(/SkcDiscordServant\\build\\.*/,`SkcDiscordServant\\database\\${fileName}`),{encoding:"utf-8"}));
+        return JSON.parse(readFileSync(`./database/${fileName}`,{encoding:"utf-8"}));
     },
     saveJSONDataInDataBase(fileName:string,data:any):void{
-        writeFileSync(__dirname.replace(/SkcDiscordServant\\build\\.*/,`SkcDiscordServant\\database\\${fileName}`),JSON.stringify(data));
+        writeFileSync(`./database/${fileName}`,JSON.stringify(data));
     }
 }
 export default helperAboutFiles;
