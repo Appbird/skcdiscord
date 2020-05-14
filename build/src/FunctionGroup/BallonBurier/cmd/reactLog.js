37f2e02d0ff47847dc219bbd279b772d0796ecdb
Appbird <project.appbird@gmail.com>
2020-05-14 15:37:40 +0900

    v1.1.1 bugfix

--- build/src/FunctionGroup/BallonBurier/cmd/reactLog.js
+++ build/src/FunctionGroup/BallonBurier/cmd/reactLog.js
@@ -4,6 +4,8 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
 };
 Object.defineProperty(exports, "__esModule", { value: true });
 var SaveDataController_1 = __importDefault(require("../Base/SaveDataController"));
+var embedMessageMaker_1 = require("../../../helper/embedMessageMaker");
+var BallonBurier_1 = __importDefault(require("../BallonBurier"));
 function reactLog(msg, tokens) {
     var saved = SaveDataController_1.default.configLoad();
     saved.idOfChannelWhichItOutputReactLogTo = msg.channel.id;
@@ -11,5 +13,10 @@ function reactLog(msg, tokens) {
         saved.idOfChannelWhichItOutputReactLogTo = "";
     }
     SaveDataController_1.default.configSave(saved);
+    msg.channel.send(embedMessageMaker_1.embedMessageMaker((saved.idOfChannelWhichItOutputReactLogTo !== "") ?
+        "このチャンネルをログ出力先チャンネルとして設定しました！" :
+        "ログを出力しないように設定しました！", BallonBurier_1.default.realFuncName, (saved.idOfChannelWhichItOutputReactLogTo !== "") ?
+        "今後このチャンネルにログが出力されます。" :
+        "どこかのチャンネルでもう一度このコマンドを入力するとログが出力されるようになります。", [], new Date(), embedMessageMaker_1.embedMsgState.Normal));
 }
 exports.default = reactLog;
--- src/FunctionGroup/BallonBurier/cmd/reactLog.ts
+++ src/FunctionGroup/BallonBurier/cmd/reactLog.ts
@@ -1,5 +1,7 @@
 import { Message} from "discord.js";
 import SaveDataController from "../Base/SaveDataController";
+import { embedMessageMaker, embedMsgState } from "../../../helper/embedMessageMaker";
+import BallonBurier from "../BallonBurier";
 
 export default function reactLog(msg:Message,tokens:string[]){
     const saved = SaveDataController.configLoad();
@@ -10,5 +12,14 @@ export default function reactLog(msg:Message,tokens:string[]){
     }
 
     SaveDataController.configSave(saved);
+    msg.channel.send(embedMessageMaker(
+        (saved.idOfChannelWhichItOutputReactLogTo !== "")?
+                "このチャンネルをログ出力先チャンネルとして設定しました！":
+                "ログを出力しないように設定しました！",
+                BallonBurier.realFuncName,
+        (saved.idOfChannelWhichItOutputReactLogTo !== "")?
+                "今後このチャンネルにログが出力されます。":
+                "どこかのチャンネルでもう一度このコマンドを入力するとログが出力されるようになります。",
+                [],new Date(),embedMsgState.Normal))
 
 }
